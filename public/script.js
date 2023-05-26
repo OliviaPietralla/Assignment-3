202175889221c6069a5b1982be71753633615e86
Olivia Pietralla <pietrallaok@gmail.com>
2023-05-26 18:46:31 +1000

    create grid

--- public/script.js
+++ public/script.js
@@ -12,3 +12,15 @@ function availability(event) {
         element.innerHTML = 'check_circle_outline';
     }
 }
+
+// POLL VOTING 
+
+const options = document.querySelectorAll("label");
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", ()=>{
+        options[i].classList.add("selected");
+        for (let j = 0; j < options.length; j++) {
+            options[j].classList.add("selectall");
+        }    
+    });
+}
