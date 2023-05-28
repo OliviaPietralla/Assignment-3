b2ef3a962b7c69a01197643c1dd117d88c91381b
Olivia Pietralla <pietrallaok@gmail.com>
2023-05-28 12:29:03 +1000

    bouncy ball

--- public/script.js
+++ public/script.js
@@ -24,3 +24,53 @@ for (let i = 0; i < options.length; i++) {
         }    
     });
 }
+
+// p5.js
+
+// let img;
+
+//function preload() {
+//  img = loadImage('netball4.png');
+//}
+
+//function setup() {
+//  createCanvas(400, 400, WEBGL);
+//  background('white')
+  
+//  image(img, 0, 0);
+//  noFill();
+//  ellipse(56, 46, 55);
+//}
+
+
+let x = 320;
+let y = 180;
+let xspeed = 4;
+let yspeed = 2;
+
+let r = 15;
+
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+}
+ 
+function draw() {
+  fill('orange')
+  background('white')
+  ellipse(x, y, r*2, r*2);
+  x += xspeed;
+  y += yspeed;
+  if(x > width - r || x < r) {
+    xspeed = -xspeed;
+  }
+  if (y > height - r || y < r) {
+    yspeed = -yspeed;
+  }
+}
+
+function mouseClicked() {
+    var d = dist(mouseX, mouseY, x, y);
+    if (d < 40) {
+        r += 5;
+    }
+}
